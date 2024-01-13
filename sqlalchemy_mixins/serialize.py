779d56b4c1b56f069c776864c15e76e648e2f8cf
bangvc <bangvc@vgroup.vn>
2024-01-13 20:21:54 +0700

    hot fix datetime

--- sqlalchemy_mixins/serialize.py
+++ sqlalchemy_mixins/serialize.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-
+from datetime import datetime, date, time
 from .inspection import InspectionMixin
 
 
@@ -8,7 +8,7 @@ class SerializeMixin(InspectionMixin):
 
     __abstract__ = True
 
-    def to_dict(self,nested = False, hybrid_attributes = False, exclude = None):
+    def to_dict(self, nested=False, hybrid_attributes=False, exclude=None):
         """Return dict object with model's data.
 
         :param nested: flag to return nested relationships' data if true
@@ -17,30 +17,36 @@ class SerializeMixin(InspectionMixin):
         :type: bool
         :return: dict
         """
-        result = dict()
 
         if exclude is None:
-             view_cols = self.columns
-        else :
-             view_cols = filter(lambda e: e not in exclude, self.columns)
+            view_cols = self.columns
+        else:
+            view_cols = filter(lambda e: e not in exclude, self.columns)
 
-        for key in view_cols :
-            result[key] = getattr(self, key)
+        result = {key: self.__validate_datetime(key) for key in view_cols}
 
         if hybrid_attributes:
             for key in self.hybrid_properties:
-                result[key] = getattr(self, key)
+                result[key] = self.__validate_datetime(key)
 
         if nested:
             for key in self.relations:
-                obj = getattr(self, key)
+                obj = self.__validate_datetime(key)
 
                 if isinstance(obj, SerializeMixin):
                     result[key] = obj.to_dict(hybrid_attributes=hybrid_attributes)
                 elif isinstance(obj, Iterable):
                     result[key] = [
-                        o.to_dict(hybrid_attributes=hybrid_attributes) for o in obj
+                        o.to_dict(hybrid_attributes=hybrid_attributes)
+                        for o in obj
                         if isinstance(o, SerializeMixin)
                     ]
 
         return result
+
+    def __validate_datetime(self, key):
+        attr = getattr(self, key)
+
+        if isinstance(attr, (datetime, date, time)):
+            return attr.__str__()
+        return attr
